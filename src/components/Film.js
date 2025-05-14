cdf98fc2d8867a8d17cda0d7a9a01e86f3d21090
Zrinka <zrinka.cvitanovic@lss.hr>
2025-05-14 15:05:57 +0200

    dvorane rade stvarno

--- src/components/Film.js
+++ src/components/Film.js
@@ -3,14 +3,6 @@ import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 
 function Film() {
-    const [filmovi, setFilmovi] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:5432/kino/film")
-            .then((resp) => resp.json())
-            .then((data) => setFilmovi(data)); // set data to state
-    }, []);
-
     return (
         <div className="App">
             <Navbar />

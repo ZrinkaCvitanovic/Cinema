8f5e2078dbb7e97e34cf3a2bd9de8d7485b41eaa
Zrinka <zrinka.cvitanovic@lss.hr>
2025-05-15 16:56:44 +0200

    brisanje pojedinačnog filma

--- src/components/Film.js
+++ src/components/Film.js
@@ -4,18 +4,69 @@ import Navbar from "./Navbar";
 
 function Film() {
     const [filmovi, setFilmovi] = useState([]);
+    const [naziv, setNaziv] = useState("");
+    const [zaposl, setZaposl] = useState("");
+    const [trajanje, setTrajanje] = useState(0);
+    const [dob, setDob] = useState(0);
+    const [cijena, setCijena] = useState(0.0);
 
     useEffect(() => {
         fetch("http://localhost:8080/api/film/all")
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
                 setFilmovi(data);
+                //console.log(data);
             })
             .catch((err) => {
                 console.log(err.message);
             });
     }, []);
+
+    const editFilm = async (id) => {
+        const response = await fetch(`http://localhost:8080/api/film/${id}`);
+        console.log(response);
+        const data = await response.json;
+        console.log(data);
+        setFilmovi(data);
+    };
+
+    const deleteFilm = async (id) => {
+        await fetch(`http://localhost:8080/api/film/${id}`, { method: "DELETE" });
+        setFilmovi(
+            filmovi.filter((film) => {
+                return film.id !== id;
+            })
+        );
+        return;
+    };
+
+    // Post with fetchAPI
+    const addFilmovi = async () => {
+        let response = await fetch(`http://localhost:8080/api/film/`, {
+            method: "POST",
+            body: JSON.stringify({
+                naziv: naziv,
+                trajanjeMin: trajanje,
+                ulazEur: cijena,
+                dobnaGranica: dob,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        });
+        let data = await response.json();
+        setFilmovi((filmovi) => [data, ...filmovi]);
+        setNaziv("");
+        setZaposl("");
+        setCijena(0.0);
+        setDob(0);
+        setTrajanje(0);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addFilmovi();
+    };
 
     return (
         <div className="App">
@@ -23,27 +74,47 @@ function Film() {
             <p>Filmovi page</p>
             <div>
                 <table>
-                    <th>ID</th>
-                    <th>naziv</th>
-                    <th>Trajanje (min)</th>
-                    <th>Cijena ulaznice (€)</th>
-                    <th>Unio</th>
-                    <th>Izmjene</th>
-                    {filmovi.map((post) => {
-                        return (
-                            <tr key={post.id}>
-                                <td className="post-title">{post.id}</td>
-                                <td className="post-title">{post.naziv}</td>
-                                <td className="post-body">{post.trajanjeMin}</td>
-                                <td>{post.ulazEur}</td>
-                                <td>{post.unioZaposlenik}</td>
-                                <td>
-                                    <button className="delete-btn">Delete</button>
-                                </td>
-                            </tr>
-                        );
-                    })}
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Naziv</th>
+                            <th>Trajanje (min)</th>
+                            <th>Cijena ulaznice (€)</th>
+                            <th>Dobna granica</th>
+                            <th>Unio</th>
+                            <th>Izmjene</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filmovi.map((film) => {
+                            return (
+                                <tr key={film.id}>
+                                    <td className="film-title">{film.id}</td>
+                                    <td className="film-title">{film.naziv}</td>
+                                    <td className="film-body">{film.trajanjeMin}</td>
+                                    <td>{film.ulazEur}</td>
+                                    <td>{film.dobnaGranica}</td>
+                                    <td>{film.unioZaposlenik}</td>
+                                    <td className="button">
+                                        <button className="delete-btn" onClick={() => deleteFilm(film.id)}>
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
                 </table>
+            </div>
+            <div className="add-post-container">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" size="70" value={naziv} placeholder="Naziv filma" onChange={(e) => setNaziv(e.target.value)} />
+                    <input type="number" value={trajanje} name="" placeholder="Trajanje u minutama" onChange={(e) => setTrajanje(e.target.value)}></input>
+                    <input type="number" value={cijena} min="0" max="20" step="0.01" placeholder="xx.xx" onChange={(e) => setCijena(e.target.value)}></input>
+                    <input type="number" value={dob} name="" onChange={(e) => setDob(e.target.value)}></input>
+                    <input type="text" value={zaposl} name="" placeholder="Korisničko ime zaposlenika" onChange={(e) => setZaposl(e.target.value)}></input>
+                    <button type="submit">Dodaj film</button>
+                </form>
             </div>
         </div>
     );
